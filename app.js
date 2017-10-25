4c2e33626995510fad79e351401204ca1b2be3cb
Dean <dean.murphy90@gmail.com>
2017-10-25 08:21:19 -0700

    made more attempts at generating a table with 0 success

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 'use strict';
 
+var table = document.getElementById('table_content');
+var stores = [];
 var cookiesSold = 0;
-var cookiesPerHour = [];
 var storeHours = ['6 AM', '7 AM', '8 AM', '9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM', '6 PM', '7 PM', '8 PM'];
 
 function Store(location, minCust, maxCust, cookiesPer) {
@@ -10,6 +11,7 @@ function Store(location, minCust, maxCust, cookiesPer) {
   this.maxCust = maxCust;
   this.cookiesPer = cookiesPer;
 }
+
 
 Store.prototype.figures = function (){
   var randomDecimal = Math.random() * (this.maxCust - this.minCust + 1) + this.minCust;
@@ -33,32 +35,77 @@ var seacenStore = new Store('Seattle Center', 11, 38, 3.7);
 var capStore = new Store('Capitol Hill', 20, 38, 2.3);
 var alkiStore = new Store('Alki', 2, 16, 4.6);
 
+stores.push(pikeStore);
+stores.push(seatacStore);
+stores.push(seacenStore);
+stores.push(capStore);
+stores.push(alkiStore);
+console.log(stores);
 
+// var arrOfSales = shopObj.salesPerHour();
 var totalCookies = 0;
+var newRow;
 
-var shopHourlySales = function(shopObj) {
-  var arrCookieHTML = [];
-  var arrOfSales = shopObj.salesPerHour();
-  console.log('arrOfSales length:', arrOfSales.length);
-
-  var storeName = document.createElement('div');
-  storeName.innerHTML = '<p>' + shopObj.location + '</p>';
-  document.body.appendChild(storeName);
-  var docData = document.createElement('ul');
+var buildHeader = function() {
+  var arrTimes = [];
   for (var i = 0; i < storeHours.length; i++) {
-    arrCookieHTML.push('<li>' + storeHours[i] + ': ' + arrOfSales[i] + '</li>');
-    totalCookies += arrOfSales[i];
+    arrTimes.push (
+      '<td>' + storeHours[i] + '</td>'
+    );
   };
-
-  arrCookieHTML.push('<li> Total: ' + totalCookies + '</li>');
-
-  var strCookieHTML = arrCookieHTML.join('');
-  docData.innerHTML = strCookieHTML;
-  document.body.appendChild(docData);
+  var tableHeader = '<tr>' + arrTimes.join('') + '</tr>'
+  var newHeader = document.createElement('thead');
+  var sales = document.getElementById('sales');
+  newHeader.innerHTML = tableHeader;
+  sales.appendChild(newHeader);
+  console.log(tableHeader);
+  console.log('arrTimes:', arrTimes);
 };
 
-shopHourlySales(pikeStore);
-shopHourlySales(seatacStore);
-shopHourlySales(seacenStore);
-shopHourlySales(capStore);
-shopHourlySales(alkiStore);
+var buildRow = function(){
+  var storeNames = [];
+  for (var j = 0; j < storeHours.length; j++) {
+    storeNames.push (
+      '<tr>' +
+      '<td>' + this.cookiesPerHour[j] + '</td>' +
+      '</tr>'
+
+    );
+  };
+  var tableNames = '<tr>' + storeNames.join('') + '</tr>'
+  var newRow = document.createElement('tr');
+  var names = document.getElementById('sales');
+  newRow.innerHTML = tableNames;
+  names.appendChild(newRow);
+  console.log('store names:', storeNames);
+};
+// var shopHourlySales = function(shopObj) {
+//   var arrCookieHTML = [];
+//   var arrTimes = [];
+//   var arrOfSales = shopObj.salesPerHour();
+//   console.log('arrOfSales length:', arrOfSales.length);
+//
+//   for (var i = 0; i < storeHours.length; i++) {
+//     arrTimes.push (
+//       '<tr>' + storeHours[i] + '</tr>'
+//     );
+//   };
+//   console.log('arrTimes:', arrTimes);
+//
+//   var newRow;
+//
+//   for (var j = 0; j < arrTimes.length; j++) {
+//     newRow = document.createElement('tr');
+//     newRow.innerHTML = arrTimes[j];
+//     table.appendChild(newRow);
+//   };
+// };
+//
+// shopHourlySales(pikeStore);
+// shopHourlySales(seatacStore);
+// shopHourlySales(seacenStore);
+// shopHourlySales(capStore);
+// shopHourlySales(alkiStore);
+
+buildHeader();
+buildRow();
